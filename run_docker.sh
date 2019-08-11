42c0b4fb19f913ead7544ae6bec49442457ac484
call900913 <call900913@icloud.com>
2019-08-11 11:38:11 -0400

    Complete run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,11 @@
-# Build, List, and Run steps are to be completed with this script.
+#!/usr/bin/env bash
+# Build, List, and Run steps are to be completed with this script
 
+# Build image and add a descriptive tag
+docker build --tag=api
 
+# List docker images
+docker image ls
+
+# Run app
+docker run -p 8000:80 api
