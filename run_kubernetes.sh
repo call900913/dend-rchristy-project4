4a1f496bb8c5901e31273a8d6528878db28eb63e
call900913 <call900913@icloud.com>
2019-08-12 14:41:23 -0400

    Complete run_kubernetes.sh

---
+++ run_kubernetes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# dockerpath=call900913/api
+
+# Run the Docker Hub Container with Kubernetes
+kubectl run klm --image=docker.io/call900913/api:v1 --port=80
+
+# List the Pods
+kubectl get pods
+
+# Forward the container port to a host
+kubectl proxy --port=8000
