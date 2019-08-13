f63d2bf773505abd64334d3ad92fb1afddaae707
call900913 <call900913@icloud.com>
2019-08-13 09:40:24 -0400

    Add a log statement to app.py /predict section

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
+    # Log of prediction value - comment and line by call900913.
+    LOG.info(f"Prediction: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
